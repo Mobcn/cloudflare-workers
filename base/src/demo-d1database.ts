6853a0975b036c7b8aa3521836de8a05eb8fcb06
Mobcn <mobingcn@gmail.com>
2025-01-24 17:24:02 +0800

    update:集成Prisma ORM框架

--- base/src/demo-d1database.ts
+++ base/src/demo-d1database.ts
@@ -1,14 +1,30 @@
 import { Hono } from 'hono';
+import { PrismaClient } from '@prisma/client';
+import { PrismaD1 } from '@prisma/adapter-d1';
+
+declare module 'hono' {
+	interface ContextVariableMap {
+		prisma: PrismaClient<{ adapter: PrismaD1 }>;
+	}
+}
 
 const d1 = new Hono<{ Bindings: Env }>();
+
+d1.use('*', async (c, next) => {
+	const adapter = new PrismaD1(c.env.DB);
+	const prisma = new PrismaClient({ adapter, log: ['query', 'info', 'warn'] });
+	c.set('prisma', prisma);
+	await next();
+});
 
 d1.get('/api/customer/search', async (c) => {
 	const name = c.req.query('name');
 	if (!name) {
 		return c.text('no param [name]!');
 	}
-	const { results } = await c.env.DB.prepare('SELECT * FROM Customers WHERE CompanyName = ?').bind(name).all();
-	return c.json(results);
+	const prisma = c.get('prisma');
+	const findList = await prisma.customers.findMany({ where: { CompanyName: { contains: name } } });
+	return c.json(findList);
 });
 d1.get('*', (c) => c.text('Call /d1/api/customer/search to see everyone who works at Bs Beverages'));
 
